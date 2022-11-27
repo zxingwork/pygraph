2a77f5cbf5d19e51ec0c99be1bf0b469f1380762
zxingwork <zxingwork@163.com>
2022-11-27 11:53:04 +0800

    init

--- graph.py
+++ graph.py
@@ -32,6 +32,28 @@ class Graph(Generic[V]):
         self._edges.append([])
         return self.vertex_count - 1
 
-    def add_edge(self, edge:Edge):
+    def add_edge(self, edge: Edge):
+        self._edges[edge.u].append(edge)
+        self._edges[edge.v].append(edge.reversed())
 
-        pass
+    def add_edge_by_indices(self, u: int, v: int):
+        edge: Edge = Edge(u, v)
+        self.add_edge(edge)
+
+    def add_edge_by_vertices(self, first: V, second: V):
+        u: int = self._vertices.index(first)
+        v: int = self._vertices.index(second)
+        self.add_edge_by_indices(u, v)
+
+    def vertex_at(self, index: int):
+        return self._vertices[index]
+
+    def index_of(self, vertex: V):
+        return self._vertices.index(vertex)
+
+    def neighbors_for_index(self, index: int):
+        return list(map(self.vertex_at, [e.v for e in self._edges[index]]))
+
+    def neighbors_for_vertex(self, vertex: V):
+        return self.neighbors_for_index(self.index_of(vertex))
+
